d3e053f79bec82f018cec1614bc1a8770b4fc9ea
chay-1 <08230187.sce@rub.edu.bt>
2025-09-23 21:28:29 -0700

    Modify nestedFunc.js

---
+++ nestedFunc.js
@@ -0,0 +1,14 @@
+function outerFun(a) {
+   console.log("The value of a is: " + a);
+   function innerFun(b) {
+       console.log("The value of b is: " + b);
+       return a + b;
+   }
+   return innerFun;
+}
+
+
+const addTen = outerFun(10);  
+console.log(addTen(5)); 
+
+
